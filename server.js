371016e2a5d92587d0fcd632c1f6ba47ef7d7ff3
Jeff Lebowski <anentitymusic@gmail.com>
2021-05-17 17:14:47 -0400

    sql connection and beginning prmopt

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
 const inquirer = require('inquirer')
 const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+	host: 'localhost',
+
+	port: 3306,
+
+	user: 'root',
+
+	password: 'FunkyMonkey68',
+	database: 'greatBayDB',
+})
+
+function start() {
+	inquirer.prompt(
+		{
+			type: 'list',
+			message: 'What would you like to do?',
+			name: 'firstPrompt',
+			choices: []
+
+	})
+}
