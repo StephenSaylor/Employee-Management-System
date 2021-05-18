520fc87f757145ec2a5501c947caf94dae46a05f
Jeff Lebowski <anentitymusic@gmail.com>
2021-05-18 18:55:55 -0400

    seeds

---
+++ seeds.sql
@@ -0,0 +1,17 @@
+
+INSERT INTO department (dept_name) 
+VALUES ('Front End'), ('Back End'), ('Management');
+
+INSERT INTO company_role (title, salary, dept_id) 
+VALUES ('Head Manager', 90000.00, 3), 
+('Head Chef', 50000.00, 2),
+('Cook', 30000.00, 2),   
+('Dishwasher', 20000.00, 2)             
+('Floor Manager', 50000.00, 1),
+('Server', 30000.00, 1),
+('Hostess', 20000.00, 1);
+
+INSERT INTO employees (first_name, last_name, emp_role_id, manager_id) 
+VALUES ('Adam', 'Anderson', 1, null),
+('Bill', 'Brasky', 6, 1),
+('Chris', 'Carpenter', 2, null);
--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const inquirer = require('inquirer')
 const mysql = require('mysql')
+const table = require('console.table')
+
 
 const connection = mysql.createConnection({
 	host: 'localhost',
-
 	port: 3306,
-
 	user: 'root',
-
 	password: 'FunkyMonkey68',
 	database: 'companyDB',
 })
 
-function start() {
+connection.connect((err) => {
+	if (err) throw err;
+	start();
+  });
+
+const start = () => {
 	inquirer.prompt(
 		{
 			type: 'list',
@@ -41,6 +45,12 @@ function start() {
 	}
 	)}
 
+const viewEmployees = () => {
+
+}
+	
+
+
 const addEmployee = () => {
 	const questions = [
 		{
@@ -62,7 +72,25 @@ const addEmployee = () => {
             choices: 
         }
     ]
-}
+	inquirer.prompt(questions)
+		.then((answers) => {
+			var roleId = null;
+            for(var i= 0; i < rolesResults.length; i++) {
+                if(rolesResults[i].title === answers.role) {
+                    roleId = rolesResults[i].role_id
+                }
+            }
+			connection.query("INSERT INTO employees SET ?",
+                {
+                    first_name: answers.firstName,
+                    last_name: answers.lastName,
+                    emp_role_id: roleId
+                },
+			function(err, results) {
+                if(err) throw err;
+                console.log("Added " + answers.firstName + " " + answers.lastName )
+			})
+}	
 	
 
 
